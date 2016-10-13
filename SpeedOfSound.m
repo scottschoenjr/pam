a651bc0d9747405b3b62e6110d444280b9737c0e
Schoen <sschoen3@gatech.edu>
2016-10-13 13:49:43 -0400

    Updated Speed Of Sound
    Added citation and enabled vector input/output

--- SpeedOfSound.m
+++ SpeedOfSound.m
@@ -1,7 +1,32 @@
-%% speed of sound calculation
+%**************************************************************************
+%
+% Speed of Sound in Water
+%
+%   Function returns the speed of sound in water for the given input
+%   temperature, calculated from Eq. (1) of "Speed of Sound in Pure Water",
+%   De Grosso and Mader JASA (52)1442, 1972.
+%
+% Input
+%   temp_degC - Array of water temperatures [deg C]
+%
+% Output
+%   soundSpeed - Sound speed [m/s]
+%
+%**************************************************************************
 
-function sos=SpeedOfSound(Temperature)
+function [ soundSpeed ] = SpeedOfSound( temp_degC )
 
-sos=1402.385 + 5.038813*Temperature - 0.05799136 *(Temperature)^2 ...
-+ 0.0003287156*(Temperature)^3 - 0.000001398845*(Temperature)^4 ...
-+ 0.00000000278786*(Temperature)^5;
+% Coefficients
+k0 = 1402.385;
+k1 = 5.038813;
+k2 = -0.05799136;
+k3 = 0.0003287156;
+k4 = -0.000001398845;
+k5 = 0.00000000278786;
+
+% Compute sound speed
+T = temp_degC;
+soundSpeed = ... % Eq.(1), values from Table III (p. 1443)
+    k0 + k1.*T + k2.*T.^(2) + k3.*T.^(3) + k4.*T.^(4) + k5.*T^(5);
+
+end
